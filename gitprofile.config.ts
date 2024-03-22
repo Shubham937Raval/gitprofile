34c98661fb2ec5b46659115666fcb72019ee32d2
Shubham Raval <79586633+Shubham937Raval@users.noreply.github.com>
2024-03-22 08:25:07 +0530

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -2,7 +2,7 @@
 
 const CONFIG = {
   github: {
-    username: 'Shubham973Raval', // Your GitHub org/user name. (This is the only required config)
+        username: 'Shubham937Raval', // Your GitHub org/user name. (Required)
   },
   /**
    * If you are deploying to https://<USERNAME>.github.io/, for example your repository is at https://github.com/arifszn/arifszn.github.io, set base to '/'.
